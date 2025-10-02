4f0057cdaafe129e3f943c7aece26004b64883bd
Wallrisss <Robbyrev7@gmail.com>
2025-10-02 13:41:39 +0000

    box

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,9 @@
 import "../styles/styles.css";
+import "../styles/spinner.css";
 import { startRouter } from "./router.js";
+import { startSpinner } from "./utils/spinner.js";
 
 /* Start the router (this lets us show different pages -- see 
 router.js) */
 startRouter();
+startSpinner();
--- src/scripts/utils/renderMenu.js
+++ src/scripts/utils/renderMenu.js
@@ -5,7 +5,23 @@ import { gsap } from "gsap/gsap-core";
 // src/scripts/utils/renderMenu.js
 export function renderMenu() {
   /* html */
-  return `    
+  return `   
+  <div class="spinner-section">
+  <button class="stop-btn">Stop</button>
+  <div class="wrapper">
+    <div class="box"><div class="box__inner"><p>1</p></div></div>
+    <div class="box"><div class="box__inner"><p>2</p></div></div>
+    <div class="box"><div class="box__inner"><p>3</p></div></div>
+    <div class="box"><div class="box__inner"><p>4</p></div></div>
+    <div class="box"><div class="box__inner"><p>5</p></div></div>
+    <div class="box"><div class="box__inner"><p>6</p></div></div>
+    <div class="box"><div class="box__inner"><p>7</p></div></div>
+    <div class="box"><div class="box__inner"><p>8</p></div></div>
+    <div class="box"><div class="box__inner"><p>9</p></div></div>
+    <div class="box"><div class="box__inner"><p>10</p></div></div>
+    <div class="box"><div class="box__inner"><p>11</p></div></div>
+  </div>
+</div> 
     <!-- code generated in renderMenu.js -->
     <nav>
       <ul>
